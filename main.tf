71c8a8dbbaefdd13088ab2504b28649a73a687c7
Jose Dalino <jrdalino@gmail.com>
2021-04-27 00:05:01 +0800

    Removed RDS Proxy init_query and session_pinning_filters

--- main.tf
+++ main.tf
@@ -215,10 +215,10 @@ resource "aws_db_proxy_default_target_group" "this" {
 
   connection_pool_config {
     connection_borrow_timeout    = 120
-    init_query                   = "SET x=1, y=2"
+    # init_query                   = "SET x=1, y=2"
     max_connections_percent      = 100
     max_idle_connections_percent = 50
-    session_pinning_filters      = ["EXCLUDE_VARIABLE_SETS"]
+    # session_pinning_filters      = ["EXCLUDE_VARIABLE_SETS"]
   }
 }
 
